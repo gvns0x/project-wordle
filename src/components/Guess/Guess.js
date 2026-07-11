94da294672bd25c0144e43429d40f0c2513ac6be
gvns0x <gvns0x@gmail.com>
2026-07-11 19:36:57 +0530

    add css logic for each letter

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -5,14 +5,13 @@ function Guess({ guessResult, answer }) {
 
   const numOfCells = Array.from({ length: 5 })
 
-  console.log(answer)
-
   return <p className="guess">
     {guessResult ?
-      [...guessResult].map(letter =>
-        <span className="cell">{letter}</span>
-      ) :
-      numOfCells.map(block => <span key={Math.random()} className='cell'></span>)
+      checkGuess(guessResult, answer).map(letterObject =>
+        <span key={Math.random()} className={`cell ${letterObject.status}`}>{letterObject.letter}</span>
+      )
+      :
+      numOfCells.map(cell => <span key={Math.random()} className='cell'></span>)
     }
   </p>;
 }
