f71b310f0f2f9b7808b11eba5ac4b1cc6f37dc4e
gvns0x <gvns0x@gmail.com>
2026-07-12 11:43:52 +0530

    function on initialization of state

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -7,12 +7,9 @@ import GuessResults from '../GuessResults/';
 import Banner from '../Banner'
 import { NUM_OF_GUESSES_ALLOWED as maxGuesses } from '../../constants';
 
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-
 function Game() {
   const [guessResults, setGuessResults] = React.useState([])
-  const [rightAnswer, setRightAnswer] = React.useState(answer)
+  const [rightAnswer, setRightAnswer] = React.useState(() => sample(WORDS))
   const gameEnded = rightAnswer === guessResults[guessResults.length - 1] || guessResults.length === maxGuesses
 
   console.log(`Answer is ${rightAnswer}`)
