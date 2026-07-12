bed8515d1906f01b7718f4eb735e295090dfa2d4
gvns0x <gvns0x@gmail.com>
2026-07-12 09:45:36 +0530

    create Cell component

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { checkGuess } from '../../game-helpers'
 
+function Cell({ status, children }) {
+
+  return (
+    <span className={`cell ${status}`}>
+      {children}
+    </span>
+  )
+}
+
 function Guess({ guessResult, rightAnswer }) {
 
   const numOfCells = Array.from({ length: 5 })
 
   return <p className="guess">
     {guessResult ?
-    // checkGuess gives an array of the ltters that match the
-    // right letter, the ones that are misplaced, and the
-    // ones that are wrong
+      // checkGuess gives an array of the ltters that match the
+      // right letter, the ones that are misplaced, and the
+      // ones that are wrong
       checkGuess(guessResult, rightAnswer).map(letterObject =>
-        <span key={Math.random()} className={`cell ${letterObject.status}`}>{letterObject.letter}</span>
+        <Cell key={Math.random()} status={letterObject.status}>{letterObject.letter}</Cell>
       )
       :
-      numOfCells.map(cell => <span key={Math.random()} className='cell'></span>)
+      numOfCells.map(cell => <Cell key={Math.random()}></Cell>)
     }
+
   </p>;
 }
 
