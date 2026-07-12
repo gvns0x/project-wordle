ee0a6ddee6f9e0328d84be710ae2fb2f03fda438
gvns0x <gvns0x@gmail.com>
2026-07-12 09:53:29 +0530

    redesign guess component logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -13,19 +13,20 @@ function Cell({ status, children }) {
 function Guess({ guessResult, rightAnswer }) {
 
   const numOfCells = Array.from({ length: 5 })
+  
+  // checkGuess gives an array of the ltters that match the
+  // right letter, the ones that are misplaced, and the
+  // ones that are wrong
+  const checkResult = checkGuess(guessResult, rightAnswer)
 
   return <p className="guess">
-    {guessResult ?
-      // checkGuess gives an array of the ltters that match the
-      // right letter, the ones that are misplaced, and the
-      // ones that are wrong
-      checkGuess(guessResult, rightAnswer).map(letterObject =>
-        <Cell key={Math.random()} status={letterObject.status}>{letterObject.letter}</Cell>
-      )
-      :
-      numOfCells.map(cell => <Cell key={Math.random()}></Cell>)
-    }
-
+    {numOfCells.map((cell, index) =>
+      <Cell
+        status={checkResult ? checkResult[index].status : null}
+      >
+        {checkResult ? checkResult[index].letter : null}
+      </Cell>
+    )}
   </p>;
 }
 
